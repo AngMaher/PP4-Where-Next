956430b0547d439f7e8aa716b4a8b0a9ebbffd41
AngMaher <angelamaher@mail.com>
2023-03-27 09:31:29 +0000

    install allauth and set up authorisation

--- wherenext/settings.py
+++ wherenext/settings.py
@@ -41,13 +41,23 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.sites',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
     'cloudinary',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
     'travelblog',
     'bucketlist',
 ]
+
+SITE_ID = 1
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
