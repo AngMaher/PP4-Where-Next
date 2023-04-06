c8880839143059d1ddb1089950b89d42f8fd8ca8
AngMaher <angelamaher@mail.com>
2023-04-06 15:03:30 +0000

    add url for update list item in bucketlist.html

--- bucketlist/urls.py
+++ bucketlist/urls.py
@@ -4,5 +4,5 @@ from .views import UserList, AddListItem, UpdateListItem
 urlpatterns = [
     path('bucketlist/', UserList.as_view(), name='bucketlist'),
     path('add_list_item/', AddListItem.as_view(), name="add_list_item"),
-    path('bucketlist/update_list_item/<int:pk>', UpdateListItem.as_view(), name="update_list_item"),
+    path('update_list_item/<int:pk>', UpdateListItem.as_view(), name="update_list_item"),
 ]
